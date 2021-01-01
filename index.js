b75b565c9fe4344d642c311d82ac6d44765f34ad
pnofrc <pnofrc@MacBook-Pro-di-pnofrc.local>
2021-01-01 21:53:58 +0100

    nuova frase che gira

--- index.js
+++ index.js
@@ -76,29 +76,29 @@ function setSceneHeight() {
   document.documentElement.style.setProperty("--viewportHeight", height);
 }
 
-function moveCameraAngle(event) {
-  const xGap =
-    (((event.clientX - window.innerWidth / 2) * 100) /
-      (window.innerWidth / 2)) *
-    -1;
-  const yGap =
-    (((event.clientY - window.innerHeight / 2) * 100) /
-      (window.innerHeight / 2)) *
-    -1;
-  const newPerspectiveOriginX =
-    perspectiveOrigin.x + (xGap * perspectiveOrigin.maxGap) / 100;
-  const newPerspectiveOriginY =
-    perspectiveOrigin.y + (yGap * perspectiveOrigin.maxGap) / 40;
+// function moveCameraAngle(event) {
+//   const xGap =
+//     (((event.clientX - window.innerWidth / 2) * 100) /
+//       (window.innerWidth / 2)) *
+//     -1;
+//   const yGap =
+//     (((event.clientY - window.innerHeight / 2) * 100) /
+//       (window.innerHeight / 2)) *
+//     -1;
+//   const newPerspectiveOriginX =
+//     perspectiveOrigin.x + (xGap * perspectiveOrigin.maxGap) / 100;
+//   const newPerspectiveOriginY =
+//     perspectiveOrigin.y + (yGap * perspectiveOrigin.maxGap) / 40;
 
-  document.documentElement.style.setProperty(
-    "--scenePerspectiveOriginX",
-    newPerspectiveOriginX
-  );
-  document.documentElement.style.setProperty(
-    "--scenePerspectiveOriginY",
-    newPerspectiveOriginY
-  );
-}
+//   document.documentElement.style.setProperty(
+//     "--scenePerspectiveOriginX",
+//     newPerspectiveOriginX
+//   );
+//   document.documentElement.style.setProperty(
+//     "--scenePerspectiveOriginY",
+//     newPerspectiveOriginY
+//   );
+// }
 
 
 
